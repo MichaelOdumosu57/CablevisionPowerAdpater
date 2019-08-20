3d65311bd530e6a6f3cc4a0a72ebb497788d678f
Michael Odumosu <michaelodumosu57@gmail.com>
2019-08-20 05:28:41 -0400

    able to prase out and replace values in srcset to get back full website functionality

--- myJS/xhrconverter.js
+++ myJS/xhrconverter.js
@@ -15,17 +15,8 @@ while(   i !== document.all.length -1   ){
     
     
     if(   typeof document.all[i].href === 'string'   ){
-        
-        
-        if(   document.all[i].href.indexOf("maps") !== -1   ){
-         
+    
             
-            console.log(document.all[i])
-            
-            
-        }
-        
-        
         if(   document.all[i].href.indexOf("http://windsorempire.com") !== -1   ){
             
             
@@ -41,20 +32,31 @@ while(   i !== document.all.length -1   ){
     if(   typeof document.all[i].src === 'string'   ){
         
         
-        if(   document.all[i].src.indexOf("maps") !== -1   ){
-         
-            
-            console.log(document.all[i])
-            
-            
-        }
-        
-        
         if(   document.all[i].src.indexOf("http://windsorempire.com") !== -1   ){
             
             
             document.all[i].src = neededPath + document.all[i].src.split("/")[document.all[i].src.split("/").length-1]
         
+        
+        }
+        
+        
+    }
+    
+    
+    if(   typeof document.all[i].srcset === 'string'   ){
+        
+        
+        if(   document.all[i].srcset.indexOf("http://windsorempire.com") !== -1   ){
+            
+            
+            var srcsetArray = document.all[i].srcset.split(",")
+            var srcsetNew = "";
+            srcsetArray.forEach((a,b)=>{
+                srcsetNew += neededPath + a.split("/")[a.split("/").length-1]+ ", "
+            })
+            document.all[i].srcset = srcsetNew
+            console.log(   document.all[i].srcset   )
         
         }
         
