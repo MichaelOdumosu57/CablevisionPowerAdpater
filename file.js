40db310fcf38768333a3a0091fa6c514593a1d88
Michael Odumosu <michaelodumosu57@gmail.com>
2019-08-21 16:26:04 -0400

    working on deploying the app on heroku properly

--- file.js
+++ file.js
@@ -21,6 +21,24 @@ const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 
+/* routing logic*/ //{
+if(   req.url.indexOf('/CablevisionPowerAdpater/dependencies/index/') !== -1   ){
+    
+    
+    if(   req.url.indexOf('css') !== -1){
+        
+        
+        res.setHeader('Content-Type', 'text/css');
+        
+	    
+    }
+    
+    
+	res.end(   fs.readFileSync(   path.join(__dirname,'dependencies','index',req.url.split("/")[req.url.split("/").length-1] )   )   )
+	
+}
+// }  /**/
+
 res.end('<h1>'+ req.url +'</h1>')
 // res.end(   fs.readFileSync(   path.join(   __dirname,'index.html'   )   )   );
 /* code that helps see where your files are */ //{
