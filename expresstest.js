e2a692eec890a9a65ee91230ef551a06e8c4a72d
Michael Odumosu <michaelodumosu57@gmail.com>
2019-08-21 18:52:39 -0400

    adding a procfile

--- expresstest.js
+++ expresstest.js
@@ -6,4 +6,19 @@ app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: ‘World’}));
 });
 
+/* code that helps see where your files are */ //{
+var filesList = ''
+fs.readdir(__dirname , (err,files) => {
+	if (err) throw err
+
+
+	else{
+		files.map((x)=>{
+		    filesList += x
+		})
+        res.end(fileList)
+	}
+})
+// }  /**/
+
 app.listen(port, () => console.log(`${file_name} app listening on port ${port}!`))
