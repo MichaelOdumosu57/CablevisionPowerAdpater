840ac95e69d771f97f7cc697169ba443fc5c25ef
Michael Odumosu <michaelodumosu57@gmail.com>
2019-08-21 14:41:32 -0400

    trying to understand how node works in heroku

--- file.js
+++ file.js
@@ -1,10 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 3000
 
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>'+__dirname+'</h1>');
+    
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+var filesList = ''
+fs.readdir(__dirname , (err,files) => {
+	if (err) throw err
+
+
+	else{
+		files.map((x)=>{
+		    filesList += x
+		})
+		res.end('<h1>'+filesList+'</h1>');
+	}
+})
+
+  
 });
 
 server.listen(port,() => {
