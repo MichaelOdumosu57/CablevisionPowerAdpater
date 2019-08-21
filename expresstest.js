ae51aaded2f3f8c6aaf9479ccce756a1437ddc17
Michael Odumosu <michaelodumosu57@gmail.com>
2019-08-21 19:01:26 -0400

    trying to get express to work with heroku

--- expresstest.js
+++ expresstest.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4200 // angular port rmbr to change this
+const file_name = __filename.split("/")[__filename.split("/").length-1].split(".js")[0]
 
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
